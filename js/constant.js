8bc1956643140fcd7642ed22c05f4eaeee0fafb8
Serhiy Walker <zavzyat@gmail.com>
2018-10-25 19:59:20 +0300

    CP-92 Fixed images for liveblogs

--- js/constant.js
+++ js/constant.js
@@ -420,7 +420,7 @@ I decided to change this and discovered a new world that surrounds me! Today I s
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>`
-        , facebookPhotoPattern: `<center>%s</center>`
+        , facebookPhotoPattern: `<center>![](%s)</center>`
         , facebookPostBodyPattern: `%s
 
 %s
